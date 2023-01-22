c619cd3ff1301789c7f7cb4dde0e9bbdfa280104
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-01-22 17:17:11 +0300

    implemented routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import './App.css';
-import FetchBots from './components/FetchBots';
+import React from "react";
+import "./App.css";
+import FetchBots from "./components/FetchBots";
+import { Routes, Route } from "react-router-dom";
+import BotDetails from "./components/BotDetails";
 
 function App() {
   return (
-    
     <div className="App">
-      <FetchBots/>
-     
+      <FetchBots />
+      <Routes>
+        <Route path="/Details" element = {<BotDetails />}/>
+          
+        
+      </Routes>
     </div>
   );
 }
