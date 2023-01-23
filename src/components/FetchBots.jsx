2ea3fc089a246efae764e663a85a9e915c03c671
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-01-23 17:41:27 +0300

    changed to jsx

--- src/components/FetchBots.jsx
+++ src/components/FetchBots.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "../components/css/FetchBot.css";
 import { Link } from "react-router-dom";
-
+// import BotDetails from "./BotDetails";
 
 function FetchBots() {
   const [bots, setBots] = useState([]);
+  const [enlistedBots ,setEnlistBots] = useState([])
   useEffect(() => {
     fetch("http://localhost:3000/bots")
       .then((res) => res.json())
@@ -14,13 +15,13 @@ function FetchBots() {
     <>
       {bots.map((bot) => {
         return (
-          <div key={bot.id} className="card" style={{ width: "20rem" }}>
+          <div key={bot.id} className="card m-4" style={{ width: "20rem" }}>
             <img src={bot.avatar_url} className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">Name: {bot.name}</h5>
 
-              <Link to="/Details" className="btn btn-primary">
-                View Bot Details
+              <Link to={`/bot-details/${bot.id}`}>
+                <button className="btn btn-primary">View Bot Details</button>
               </Link>
               <button className="btn btn-success">Enlist</button>
             </div>
