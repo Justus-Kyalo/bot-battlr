f39d6739048b20791b29377f4a2cde826d54f181
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-01-23 22:48:06 +0300

    added enlist btn

--- src/components/FetchBots.jsx
+++ src/components/FetchBots.jsx
@@ -1,33 +1,63 @@
 import React, { useEffect, useState } from "react";
 import "../components/css/FetchBot.css";
 import { Link } from "react-router-dom";
-// import BotDetails from "./BotDetails";
+import EnlistedBots from "./EnlistedBots";
 
 function FetchBots() {
   const [bots, setBots] = useState([]);
-  const [enlistedBots ,setEnlistBots] = useState([])
+  const [enlisted, setEnlisted] = useState([]);
+  const [counter, setCounter] = useState(1);
+
   useEffect(() => {
     fetch("http://localhost:3000/bots")
       .then((res) => res.json())
-      .then((data) => setBots(data));
+      .then((data) => {
+        const newBots = data.map(bot => {
+          bot.isEnlisted = false;
+          return bot;
+        });
+        setBots(newBots);
+      });
   }, []);
+
+
+  function handleEnlist(bot) {
+    if (bot.isEnlisted) {
+      bot.isEnlisted = false;
+      setEnlisted(enlisted.filter(b => b.id !== bot.id));
+    } else {
+      bot.isEnlisted = true;
+      bot.id = counter;
+      setCounter(counter + 1);
+      setEnlisted([...enlisted, bot]);
+    }
+    setBots([...bots]);
+  }
+
+
   return (
     <>
-      {bots.map((bot) => {
-        return (
-          <div key={bot.id} className="card m-4" style={{ width: "20rem" }}>
-            <img src={bot.avatar_url} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Name: {bot.name}</h5>
+      <EnlistedBots enlisted={enlisted} />
+      <div>
+        {bots.map((bot) => {
+          return (
+            <div key={bot.id} className="card m-4" style={{ width: "20rem" }}>
+              <img src={bot.avatar_url} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Name: {bot.name}</h5>
 
-              <Link to={`/bot-details/${bot.id}`}>
-                <button className="btn btn-primary">View Bot Details</button>
-              </Link>
-              <button className="btn btn-success">Enlist</button>
+                <Link to={`/bot-details/${bot.id}`}>
+                  <button className="btn btn-primary">View Bot Details</button>
+                </Link>
+                <button className="btn btn-success" onClick={() => { handleEnlist(bot) }}>
+                  {bot.isEnlisted ? "Enlisted" : "Enlist"}
+                </button>
+
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </>
   );
 }
