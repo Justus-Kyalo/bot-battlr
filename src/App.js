f9e623bc0699389e3fe6dd27cdbcdfcf0baef8c5
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-01-23 22:45:25 +0300

    able to route

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import "./App.css";
 import FetchBots from "./components/FetchBots";
 import { Routes, Route } from "react-router-dom";
 import BotDetails from "./components/BotDetails";
+import EnlistedBots from "./components/EnlistedBots";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
+  return (<>
+    <Navbar/>
     <Routes>
+      
       <Route path="/" element={<FetchBots />} />
       <Route path="/bot-details/:id" element={<BotDetails />} />
+      <Route path="/enlistedbots" element={<EnlistedBots />} />
     </Routes>
-  );
+    </>);
 }
 
 export default App;
